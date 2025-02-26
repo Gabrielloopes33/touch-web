a5efd746deea45e6e438ff0ff17e4d74952e0481
Gabriel <gabriel_lopes355@yahoo.com.br>
2025-02-26 11:40:55 -0300

    adição do botão de sms pro tiaguin

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -5,27 +5,7 @@ import { useCallback } from "react";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import type { Engine } from "tsparticles-engine";
-
-const links = [
-    {
-        title: "Instagram",
-        url: "https://www.instagram.com/agencia.touch",
-        bgColor: "bg-gradient-to-r from-purple-600 to-pink-600",
-        hoverColor: "hover:from-purple-700 hover:to-pink-700"
-    },
-    {
-        title: "WhatsApp",
-        url: "https://wa.me/5531997153646",
-        bgColor: "bg-gradient-to-r from-green-500 to-green-600",
-        hoverColor: "hover:from-green-600 hover:to-green-700"
-    },
-    {
-        title: "Website",
-        url: "/",
-        bgColor: "bg-gradient-to-r from-gray-700 to-gray-800",
-        hoverColor: "hover:from-gray-800 hover:to-gray-900"
-    }
-];
+import { links } from './links';
 
 const LinksPage = () => {
     const particlesInit = useCallback(async (engine: Engine) => {
