6dfb4ad02325a4a4b58d434afbbd560ec4665f14
Gabriel <gabriel_lopes355@yahoo.com.br>
2025-03-07 15:03:59 -0300

    adicionando links.tsx

---
+++ app/links/links.tsx
@@ -0,0 +1,21 @@
+"use client";
+export const links = [
+    {
+        title: "Instagram",
+        url: "https://www.instagram.com/agencia.touch",
+        bgColor: "bg-gradient-to-r from-purple-600 to-pink-600",
+        hoverColor: "hover:from-purple-700 hover:to-pink-700"
+    },
+    {
+        title: "WhatsApp",
+        url: "https://wa.me/5531997153646",
+        bgColor: "bg-gradient-to-r from-green-500 to-green-600",
+        hoverColor: "hover:from-green-600 hover:to-green-700"
+    },
+    {
+        title: "Website",
+        url: "/",
+        bgColor: "bg-gradient-to-r from-gray-700 to-gray-800",
+        hoverColor: "hover:from-gray-800 hover:to-gray-900"
+    }
+];
