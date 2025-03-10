157c996a0a67e0a7224e5a60d2ab28750cfacba3
Gabriel <gabriel_lopes355@yahoo.com.br>
2025-03-10 13:19:04 -0300

    responsivo da navbar

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,14 +1,32 @@
+"use client"
+
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import ThemeToggle from './theme-toggle'
+import ThemeToggle from './theme-toggle';
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <header className="fixed top-5 left-2 right-5 bg-white/1 z-30 py-4">
-      <div className="mx-auto max-w-6xl px-4 sm:px-6">
-        <div className="relative flex h-14 items-center justify-between rounded-2xl bg-white/90 px-3 shadow-lg shadow-black/[0.03] backdrop-blur-sm before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent dark:before:hidden before:[background:linear-gradient(theme(colors.gray.100),theme(colors.gray.200))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)]">
-          {/* Site branding */}
-          <div className="flex-shrink-0">
+    <header className="flex fixed top-5 left-2 right-5 bg-white/1 z-30 py-4 md:gap-8">
+      <div className="flex mx-auto max-w-6xl px-4 sm:px-6 md:gap-8">
+        <div className="relative flex h-14 items-center gap-10 justify-between md:gap-20 w-full rounded-2xl bg-white/90 px-3 shadow-lg shadow-black/[0.03] backdrop-blur-sm">
+          {/* Botão Hambúrguer */}
+          <button className="md:hidden" onClick={toggleMenu} aria-label="Toggle menu">
+            <span className="hamburger-icon">
+              <div className="w-6 h-0.5 bg-black mb-1"></div>
+              <div className="w-6 h-0.5 bg-black mb-1"></div>
+              <div className="w-6 h-0.5 bg-black"></div>
+            </span>
+          </button>
+
+          {/* Site Branding */}
+          <div className="flex items-center">
             <Link href="/" className="block" aria-label="Cruip">
               <Image
                 src="/images/logo-full.svg"
@@ -22,11 +40,11 @@ export default function Header() {
           </div>
 
           {/* Navigation */}
-          <nav className="flex flex-1 justify-end items-center gap-2 md:gap-4">
+          <nav className={`flex flex-col md:flex-row flex-1 justify-end items-center gap-6 md:gap-8 ${menuOpen ? "block" : "hidden"} md:block`}>
             <ThemeToggle />
             <Link
               href="/Sobre"
-              className="btn-sm bg-white text-gray-800 shadow hover:bg-gray-50 whitespace-nowrap text-sm"
+              className="btn-sm bg-white text-gray-800 shadow hover:bg-gray-50 whitespace-nowrap text-sm gap-6 md:gap-8"
             >
               Sobre
             </Link>
